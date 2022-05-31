3ec90135a9a8b44a3ca0225df18415af3b041711
lkndjrt <lkndjrt@gmail.com>
2022-05-31 14:20:56 -0300

    contracts: update strategy with latest swap connector

--- contracts/AaveStrategy.sol
+++ contracts/AaveStrategy.sol
@@ -29,20 +29,21 @@ import '@openzeppelin/contracts/utils/math/SafeMath.sol';
 import './aave/ILendingPool.sol';
 
 contract AaveStrategy is IStrategy, Ownable {
-    using FixedPoint for uint256;
     using SafeERC20 for IERC20;
+    using FixedPoint for uint256;
 
+    uint256 private constant MAX_SLIPPAGE = 10e16; // 10%
     uint256 private constant VAULT_EXIT_RATIO_PRECISION = 1e18;
+
+    event SetSlippage(uint256 slippage);
 
     IVault internal immutable _vault;
     IERC20 internal immutable _token;
-
-    string private _metadataURI;
-
-    uint256 internal immutable _slippage;
-
     IERC20 internal immutable _aToken;
     ILendingPool internal immutable _lendingPool;
+
+    uint256 private _slippage;
+    string private _metadataURI;
 
     modifier onlyVault() {
         require(address(_vault) == msg.sender, 'CALLER_IS_NOT_VAULT');
@@ -57,14 +58,11 @@ contract AaveStrategy is IStrategy, Ownable {
         uint256 slippage,
         string memory metadataURI
     ) {
-        require(slippage <= FixedPoint.ONE, 'SWAP_SLIPPAGE_ABOVE_1');
-
         _vault = vault;
         _token = token;
         _aToken = aToken;
         _lendingPool = lendingPool;
-        _slippage = slippage;
-
+        _setSlippage(slippage);
         _setMetadataURI(metadataURI);
     }
 
@@ -100,15 +98,12 @@ contract AaveStrategy is IStrategy, Ownable {
         return _getATokenBalance();
     }
 
-    function setMetadataURI(string memory metadataURI) external onlyOwner {
-        _setMetadataURI(metadataURI);
+    function setSlippage(uint256 newSlippage) external onlyOwner {
+        _setSlippage(newSlippage);
     }
 
-    function withdraw(IERC20 token, address recipient) external onlyOwner {
-        if (token != _token && token != _aToken) {
-            uint256 balance = token.balanceOf(address(this));
-            token.transfer(recipient, balance);
-        }
+    function setMetadataURI(string memory metadataURI) external onlyOwner {
+        _setMetadataURI(metadataURI);
     }
 
     function onJoin(uint256 amount, bytes memory)
@@ -118,7 +113,6 @@ contract AaveStrategy is IStrategy, Ownable {
         returns (uint256 value, uint256 totalValue)
     {
         invest(_token);
-
         value = amount;
         totalValue = _getATokenBalance();
     }
@@ -129,14 +123,13 @@ contract AaveStrategy is IStrategy, Ownable {
         onlyVault
         returns (address token, uint256 amount, uint256 value, uint256 totalValue)
     {
-        // Invests before exiting only if it is a normal exit
+        // Invest before exiting only if it is a non-emergency exit
         if (!emergency) {
             invest(_token);
         }
 
         amount = SafeMath.div(_getATokenBalance().mulDown(ratio), VAULT_EXIT_RATIO_PRECISION);
         _lendingPool.withdraw(address(_token), amount, address(this));
-
         _token.approve(address(_vault), amount);
 
         token = address(_token);
@@ -147,32 +140,30 @@ contract AaveStrategy is IStrategy, Ownable {
     function invest(IERC20 token) public {
         require(token != _aToken, 'AAVE_INTERNAL_TOKEN');
 
-        uint256 balance = token.balanceOf(address(this));
-        if (balance > 0) {
-            if (token != _token) {
-                _swap(token, _token, balance, getSlippage());
-            }
-            uint256 amount = _token.balanceOf(address(this));
-            _token.approve(address(_lendingPool), amount);
-            _lendingPool.deposit(address(_token), amount, address(this), 0);
+        if (token != _token) {
+            uint256 amountIn = token.balanceOf(address(this));
+            _swap(token, _token, amountIn);
         }
+
+        uint256 amount = _token.balanceOf(address(this));
+        if (amount == 0) return;
+        _token.approve(address(_lendingPool), amount);
+        _lendingPool.deposit(address(_token), amount, address(this), 0);
     }
 
-    function _swap(IERC20 tokenIn, IERC20 tokenOut, uint256 amountIn, uint256 slippage) internal returns (uint256) {
+    function _swap(IERC20 tokenIn, IERC20 tokenOut, uint256 amountIn) internal {
+        if (amountIn == 0) return;
         require(tokenIn != tokenOut, 'SWAP_SAME_TOKEN');
 
-        uint256 minAmountOut = _getMinAmountOut(tokenIn, tokenOut, amountIn, slippage);
-        ISwapConnector swapConnector = ISwapConnector(_vault.swapConnector());
-        uint256 expectedAmountOut = swapConnector.getAmountOut(address(tokenIn), address(tokenOut), amountIn);
-        require(expectedAmountOut >= minAmountOut, 'EXPECTED_SWAP_MIN_AMOUNT');
-
-        if (amountIn > 0) {
-            tokenIn.safeTransfer(address(swapConnector), amountIn);
-        }
+        IPriceOracle priceOracle = IPriceOracle(_vault.priceOracle());
+        uint256 price = priceOracle.getTokenPrice(address(tokenOut), address(tokenIn));
+        uint256 minAmountOut = amountIn.mulUp(price).mulUp(FixedPoint.ONE - _slippage);
+        address swapConnector = _vault.swapConnector();
+        tokenIn.safeTransfer(swapConnector, amountIn);
 
         uint256 preBalanceIn = tokenIn.balanceOf(address(this));
         uint256 preBalanceOut = tokenOut.balanceOf(address(this));
-        (uint256 remainingIn, uint256 amountOut) = swapConnector.swap(
+        (uint256 remainingIn, uint256 amountOut) = ISwapConnector(swapConnector).swap(
             address(tokenIn),
             address(tokenOut),
             amountIn,
@@ -186,16 +177,6 @@ contract AaveStrategy is IStrategy, Ownable {
         require(postBalanceIn >= preBalanceIn.add(remainingIn), 'SWAP_INVALID_REMAINING_IN');
         uint256 postBalanceOut = tokenOut.balanceOf(address(this));
         require(postBalanceOut >= preBalanceOut.add(amountOut), 'SWAP_INVALID_AMOUNT_OUT');
-        return amountOut;
-    }
-
-    function _getMinAmountOut(IERC20 tokenIn, IERC20 tokenOut, uint256 amountIn, uint256 slippage)
-        internal
-        view
-        returns (uint256 minAmountOut)
-    {
-        uint256 price = IPriceOracle(_vault.priceOracle()).getTokenPrice(address(tokenOut), address(tokenIn));
-        minAmountOut = amountIn.mulUp(price).mulUp(FixedPoint.ONE - slippage);
     }
 
     function _getATokenBalance() internal view returns (uint256) {
@@ -205,5 +186,11 @@ contract AaveStrategy is IStrategy, Ownable {
     function _setMetadataURI(string memory metadataURI) private {
         _metadataURI = metadataURI;
         emit SetMetadataURI(metadataURI);
+    }
+
+    function _setSlippage(uint256 newSlippage) private {
+        require(newSlippage <= MAX_SLIPPAGE, 'SLIPPAGE_ABOVE_MAX');
+        _slippage = newSlippage;
+        emit SetSlippage(newSlippage);
     }
 }
